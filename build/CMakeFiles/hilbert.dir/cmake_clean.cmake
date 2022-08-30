7740a8880d9874fcdebfda61b82c1319631f97d7
buzepeng <buzepeng@gmail.com>
2022-08-30 01:37:16 +0000

    release

---
+++ build/CMakeFiles/hilbert.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hilbert.dir/src/hilbert_generated_hilbert_oneFrame.cu.o"
+  "CMakeFiles/hilbert.dir/cmake_device_link.o"
+  "hilbert.pdb"
+  "hilbert"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/hilbert.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/hilbert_oneFrame.cu
+++ src/hilbert_oneFrame.cu
@@ -1,136 +1,222 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/core.hpp>
 #include <iostream>
+#include <stdio.h>
 #include <fstream>  
+#include <vector>
+#include <math.h>
 #include <chrono>
 #include <iomanip>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <cufft.h> 
+#include <chrono>
 #include <device_launch_parameters.h>
-#include <device_functions.h>
-#define NX 1500
-#define NY 1000
-#define NXY NX*NY
+#define NX 4000
+#define NY 4096
 #define TILEW 16
 #define TILEH 16
+#define TAPS 128
 
-using namespace std;
+__constant__ cufftComplex H[NX];
 
-__global__ void hilbertFreqFilter(cufftComplex* signal){
-    int ix = threadIdx.x + blockIdx.x*blockDim.x;
-    int iy = threadIdx.y + blockIdx.y*blockDim.y;
-    int idx = ix + iy * NX;
-    if(ix<NX && iy<NY){
-        signal[idx].x = ((ix==0)+2*(ix>0 && ix < NX/2)+0*(ix>=NX/2 && ix < NX))*signal[idx].x;      //对判断条件进行代数运算，避免分支
-        signal[idx].y = ((ix==0)+2*(ix>0 && ix < NX/2)+0*(ix>=NX/2 && ix < NX))*signal[idx].y;
-        __syncthreads();
-    }
+__device__ cufftComplex operator * (cufftComplex a, cufftComplex b){
+    cufftComplex res;
+    res.x = (a.x*b.x - a.y*b.y);
+    res.y = (a.x*b.y + a.y*b.x);
+    return res;
 }
 
-__global__ void computEnvelope(float* origin, cufftComplex* filtered){
-    int ix = threadIdx.x + blockIdx.x*blockDim.x;
-    int iy = threadIdx.y + blockIdx.y*blockDim.y;
-    int idx = ix + iy * NX;
+__global__ void FreqDomainMul(cufftComplex* signals){
+    int ix = blockDim.x*blockIdx.x+threadIdx.x;
+    int iy = blockDim.y*blockIdx.y+threadIdx.y;
+    int idx = iy*NX+ix;
+    int tile_idx = threadIdx.y*TILEW+threadIdx.x;
 
-    if(ix<NX && iy<NY){
-        float yi = origin[idx] - filtered[idx].x/NX;    //cufft没有考虑信号的尺度变换
-        origin[idx] = abs(yi);
-        __syncthreads();
-    }
+    __shared__ cufftComplex filter_s[TILEW], signals_s[TILEW*TILEH];
+    filter_s[threadIdx.x] = H[ix];
+    signals_s[tile_idx] = signals[idx];
+    __syncthreads();
+
+    signals_s[tile_idx] = signals_s[tile_idx] * filter_s[threadIdx.x];
+    signals[idx] = signals_s[tile_idx];
+    __syncthreads();
 }
 
-cv::Mat hilbert(   float* signals,
-                cufftReal* d_inputData,
-                cufftComplex* d_fftOutput,
-                cufftHandle planForward,
-                cufftHandle planInverse
-){
-    //将数据拷贝到显存，执行FFT正变换
-    cudaMemcpy(d_inputData, (cufftReal*)signals, NXY * sizeof(cufftReal), cudaMemcpyHostToDevice);
-    cufftExecR2C(planForward, d_inputData, d_fftOutput);
-    dim3 block(TILEH, TILEW, 1);
-    dim3 grid((NX-block.x)/block.x+1, (NY-block.y)/block.y+1, 1);
-    //希尔伯特频域滤波
-    hilbertFreqFilter<<<grid, block>>>(d_fftOutput);
-    cudaDeviceSynchronize();
-    //执行FFT反变换
-    cufftExecC2C(planInverse, d_fftOutput, d_fftOutput, CUFFT_INVERSE);
-    cudaDeviceSynchronize();
-    //计算信号包络
-    computEnvelope<<<grid, block>>>(d_inputData, d_fftOutput);
-    cudaDeviceSynchronize();
-    //将显存中的数据拷贝至内存，并进行设备主机同步
-    cv::Mat result = cv::Mat::zeros(NY, NX, CV_32FC1);
-    cudaMemcpy(signals, d_inputData, sizeof(float)*NXY, cudaMemcpyDeviceToHost);
-    memcpy(result.ptr<float>(0), signals, sizeof(float)*NXY);
-    cudaDeviceSynchronize();
-    return result;
+__global__ void Real2Complex(cufftReal* in, cufftComplex* out){
+    int ix = blockDim.x*blockIdx.x+threadIdx.x;
+    int iy = blockDim.y*blockIdx.y+threadIdx.y;
+    int idx = iy*NX+ix;
+
+    out[idx].x = in[idx];
+    out[idx].y = 0;
+    __syncthreads();
 }
+
+__global__ void normalization(cufftReal* in){
+    int ix = blockDim.x*blockIdx.x+threadIdx.x;
+    int iy = blockDim.y*blockIdx.y+threadIdx.y;
+    int idx = iy*NX+ix;
+
+    in[idx] = in[idx]/(2*NX);
+    __syncthreads();
+}
+
+__global__ void warmup(){
+
+}
+
+static float sinc(const float x)
+{
+    if (x == 0)
+        return 1;
+
+    return sin(M_PI * x) / (M_PI * x);
+}
+
+class HilbertBandPassFilter{
+    private:
+        float *input, *h;
+        cufftReal *d_inputData, *d_h_in;
+        cufftComplex *d_fftOutput, *d_h_out;
+        cufftHandle planForward, planInverse, planFilter;
+    public:
+        HilbertBandPassFilter(float f1, float f2){
+
+            // cudaFree(0);
+            warmup<<<1,1>>>();
+            cudaMallocHost((void**)&input, (size_t)sizeof(float)*NX*NY);
+            cudaMallocHost((void**)&h, (size_t)sizeof(float)*NX);
+            cudaMalloc((void**) &d_h_in, NX * sizeof(cufftReal));
+	        cudaMalloc((void**) &d_h_out, NX * sizeof(cufftComplex));
+            cudaMalloc((void**) &d_inputData, NX * NY * sizeof(cufftReal));
+	        cudaMalloc((void**) &d_fftOutput, NX * NY * sizeof(cufftComplex));
+
+            // int rank=1, istride=1, idist = NX, ostride=1, odist = NX;
+            // int n[1] = {NX}, inembed[2] = {NY, NX}, onembed[2] = {NY, NX};
+            int rank=1;
+            int n[1];
+            n[0]=NX;
+            int istride=1;
+            int idist = NX;
+            int ostride=1;
+            int odist = NX;
+            int inembed[2];
+            int onembed[2];
+            inembed[0]=NX;  onembed[0]=NX;
+            inembed[1] = NY; onembed[0] = NY;
+
+            // cufftPlanMany(&planForward,rank,n,inembed, istride ,idist , onembed, ostride,odist, CUFFT_R2C, NY);
+            cufftPlanMany(&planForward,rank,n,inembed, istride ,idist , onembed, ostride,odist, CUFFT_R2C, NY);
+            cufftPlanMany(&planInverse,rank,n,inembed, istride ,idist , onembed, ostride,odist, CUFFT_C2R, NY);
+            cufftPlan1d(&planFilter, NX, CUFFT_R2C, 1);
+
+            //generate bandpas filter
+            memset(h, 0, NX);
+            for(int i = 0; i < TAPS; i++) {
+                int n = i - int(TAPS/2);
+                float w =  sin(((float) M_PI * i) / (TAPS - 1)) *
+                        sin(((float) M_PI * i) / (TAPS - 1));
+                h[i] = 2.0*f1*sinc(2.0*f1*n) - 2.0*f2*sinc(2.0*f2*n);
+                h[i] = w * h[i];
+            }
+
+            cudaMemcpy(d_h_in, (cufftReal*)h, NX * sizeof(cufftReal), cudaMemcpyHostToDevice);
+            cufftExecR2C(planFilter, d_h_in, d_h_out);
+            cudaMemcpyToSymbol( H, d_h_out, sizeof(cufftComplex)*NX, 0, cudaMemcpyDeviceToDevice );
+
+            //generate hilbert filter
+            for(int i = 0;i<NX;i++){
+                if(i == 0 || (float)i == NX/2.0){
+                    h[i] = 1;
+                }else if(i<NX/2){
+                    h[i] = 2;
+                }else{
+                    h[i] = 0;
+                }
+            }
+            cudaMemcpy(d_h_in, (cufftReal*)h, NX * sizeof(cufftReal), cudaMemcpyHostToDevice);
+            dim3 block(TILEW, 1, 1);
+            dim3 grid(ceil(NX/block.x), 1, 1);
+            Real2Complex<<<grid, block>>>(d_h_in, d_h_out);
+            FreqDomainMul<<<grid, block>>>(d_h_out);
+            cudaMemcpyToSymbol( H, d_h_out, sizeof(cufftComplex)*NX, 0, cudaMemcpyDeviceToDevice );
+        }
+        float* read_data(std::string file_path){
+            std::ifstream fp_input;
+            fp_input.open(file_path, std::ios::in);
+            if (!fp_input) { //打开失败
+                std::cout << "error opening source file." << std::endl;
+                return nullptr;
+            }
+            for(int i = 0;i<NY;i++){
+                for(int j = 0;j<NX; j++){
+                    fp_input>>input[i*NX+j];
+                }
+            }
+            fp_input.close();
+            return input;
+        }
+
+        float* filter(float* signals){
+            cudaMemcpy(d_inputData, (cufftReal*)signals, NX * NY * sizeof(cufftReal), cudaMemcpyHostToDevice);
+            cufftExecR2C(planForward, d_inputData, d_fftOutput);
+            dim3 block(TILEW, TILEH, 1);
+            dim3 grid(ceil(NX/block.x), ceil(NY/block.y), 1);
+            FreqDomainMul<<<grid, block>>>(d_fftOutput);
+            //执行FFT反变换
+            cufftExecC2R(planInverse, d_fftOutput, d_inputData);
+            normalization<<<grid, block>>>(d_inputData);
+            cudaMemcpy(input, (float*)d_inputData, (size_t)sizeof(float)*NX*NY, cudaMemcpyDeviceToHost);
+            cudaDeviceSynchronize();
+            return input;
+        }
+
+        void save_result(float* result, std::string filepath){
+            std::ofstream fp_output;
+            fp_output.open(filepath, std::ios::out);
+            if (!fp_output) {
+                std::cout << "error opening destination file." << std::endl;
+                return;
+            }
+            for(int i = 0;i<NY;i++){
+                for(int j = 0;j<NX;j++){
+                    // std::cout<<result[i*NX+j]<<" "<<std::endl;
+                    if(j!=NX-1){
+                        fp_output<<result[i*NX+j]<<' ';
+                    }else{
+                        fp_output<<result[i*NX+j]<<'\n';
+                    } 
+                }
+            }
+            fp_output.close();
+        }
+        ~HilbertBandPassFilter(){
+            cufftDestroy(planForward);
+            cufftDestroy(planInverse);
+            cufftDestroy(planFilter);
+            cudaFreeHost(input);
+            cudaFreeHost(h);
+            cudaFree(d_inputData);
+            cudaFree(d_fftOutput);
+            cudaFree(d_h_out);
+            cudaFree(d_h_in);
+        }
+};
+
 
 int main(){
-    //申请锁页内存和显存
-    float *input;
-    cudaMallocHost((void**)&input, (size_t)sizeof(float)*NXY);
-    cufftReal *d_inputData;
-	cufftComplex *d_fftOutput;
-	cudaMalloc((void**) &d_inputData, NXY * sizeof(cufftReal));
-	cudaMalloc((void**) &d_fftOutput, NXY * sizeof(cufftComplex));
-    //打开文件，将txt中的数据读取到input数组中
-    ifstream fp_input;
-	ofstream fp_output;
-    fp_input.open("./data/data.txt", ios::in);
-	if (!fp_input) { //打开失败
-        cout << "error opening source file." << endl;
-        return 0;
-    }
-	fp_output.open("./data/output_gpu.txt", ios::out);
-	if (!fp_output) {
-        fp_input.close(); //程序结束前不能忘记关闭以前打开过的文件
-        cout << "error opening destination file." << endl;
-        return 0;
-    }
-    for(int i = 0;i<NY;i++){
-        for(int j = 0;j<NX; j++){
-            fp_input>>input[i*NX+j];
-        }
-    }
-    //定义两次FFT的类型
-    cufftHandle planForward, planInverse;
-    int rank=1;
-	int n[1];
-	n[0]=NX;
-	int istride=1;
-	int idist = NX;
-	int ostride=1;
-	int odist = NX;
-	int inembed[2];
-	int onembed[2];
-	inembed[0]=NX;  onembed[0]=NX;
-	inembed[1] = NY; onembed[1] = NY;
-    cufftPlanMany(&planForward,rank,n,inembed, istride ,idist , onembed, ostride,odist, CUFFT_R2C, NY);
-    cufftPlanMany(&planInverse,rank,n,inembed, istride ,idist , onembed, ostride,odist, CUFFT_C2C, NY);
-    //进行希尔伯特变换并计时
-    auto startTime = chrono::system_clock::now();
-    cv::Mat result = hilbert(input, d_inputData, d_fftOutput, planForward, planInverse);
-	auto endTime = chrono::system_clock::now();
-    cout << "gpu time:" << chrono::duration_cast<chrono::milliseconds>(endTime - startTime).count() <<"ms"<< endl;
-    for(int i = 0;i<NY;i++){
-        for(int j = 0;j<NX;j++){
-            // if(j!=NX-1) fp_output<<input[i*NX+j]<<'\t';
-            // else fp_output<<input[i*NX+j];
-            if(j!=NX-1) fp_output<<result.at<float>(i,j)<<'\t';
-            else fp_output<<result.at<float>(i,j);
-        }
-        fp_output<<'\n';
-    }
-	//释放内存，关闭文件
-	fp_input.close();
-	fp_output.close();
-    cufftDestroy(planForward);
-    cufftDestroy(planInverse);
-    cudaFreeHost(input);
-    cudaFree(d_inputData);
-    cudaFree(d_fftOutput);
+    float *signals, *results;
+    float fs = 1000, fl = 15.0/fs, fh = 45.0/fs;
+
+    HilbertBandPassFilter hf(fl, fh);
+    
+    signals = hf.read_data("/home/hilbert/data/input_data.txt");
+    std::cout<<"Signal read complete!"<<std::endl;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    results = hf.filter(signals);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout<<"Result compute complete! Time spend: " << std::chrono::duration<float>(end - start).count()<<std::endl;
+    hf.save_result(results, "/home/hilbert/data/output_data.txt");
+
 	return 0;
 }
